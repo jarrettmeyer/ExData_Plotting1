37b977032521ee52b37b2a7b24b1b67164248cde
Jarrett Meyer <jarrettmeyer@gmail.com>
2016-03-06 07:31:48 -0500

    fixed dates in readdata.R

--- readdata.R
+++ readdata.R
@@ -7,4 +7,7 @@ valid_dates <- as.Date(c("2007-02-01", "2007-02-02"))
 dat <- read.table(file, header = TRUE, sep = separator, stringsAsFactors = FALSE)
 dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
 dat <- subset(dat, Date %in% valid_dates)
+
+dat$POSIX_Date <- as.POSIXlt(paste(dat$Date, dat$Time, sep = " "))
+dat$Day <- weekdays(dat$POSIX_Date, abbreviate = TRUE)
 dat$Global_active_power <- as.numeric(as.character(dat$Global_active_power))
